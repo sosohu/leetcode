2dd292866374b25c02d0dca113ee7e8830d4bb4e
sosohu <ustc.sosohu@gmail.com>
2014-09-23 12:46:12 +0800

    slow BFS

--- Word_Ladder_II/main.cc
+++ Word_Ladder_II/main.cc
@@ -23,10 +23,36 @@ void Trace(vector<int>& trace, vector<string>& data, int pos, vector<vector<stri
 		path.push_back(vec);
 	}
 
+	/*
+	void detail(string start, string end, unordered_set<string>& dict, vector<string> trace, vector<vector<string> >path){
+		trace.push_back(start);
+		if(start.compare(end) == 0){
+			path.push_back(trace);
+			return;
+		}
+		int len = start.length();
+		string str;
+		for(int i = 0; i < len; i++){
+			str = start;
+			for(int j = 97; j < 123; j++){
+				str[i] = j;
+				detail(str, end, dict, trace, path);
+			}
+		}
+	}
+
+	vector<vector<string> > findLadders(string start, string end, unordered_set<string> &dict) {
+		vector<vector<string> > path;
+		vector<string> trace;
+		detail(start, end, dict, trace, path);
+		return path;
+	}
+	*/
+
 	vector<vector<string> > findLadders(string start, string end, unordered_set<string> &dict) {
 		int len = start.length();
 		unordered_set<string> find;
-		unordered_set<string> bad;
+		//unordered_set<string> bad;
 		vector<string> data;
 		vector<vector<string> > path;
 		vector<int> trace;
@@ -38,18 +64,20 @@ void Trace(vector<int>& trace, vector<string>& data, int pos, vector<vector<stri
 		int length = 0;
 		int size;
 		int pos = 0;
-		bool is_bad;
+		//bool is_bad;
+		bool this_level = false;
 		while(size = data.size() - pos){
 			unordered_set<string> local_find;
 			for(int i = 0; i < size; i++){
 				str = data[pos+i];
-				if(bad.count(str) == 1)
-					continue;
+				//if(bad.count(str) == 1)
+				//	continue;
 				if(str.compare(end) == 0){
 					Trace(trace, data , pos+i, path);
+					this_level = true;
 					continue;
 				}
-				is_bad = true;
+				//is_bad = true;
 				for(int j = 0; j < len; j++){
 				 	tmp = str;
 					for(int k = 97; k < 123; k++){
@@ -57,15 +85,17 @@ void Trace(vector<int>& trace, vector<string>& data, int pos, vector<vector<stri
 						if(dict.count(tmp) == 1 && find.count(tmp) == 0 ){
 							data.push_back(tmp);
 							trace.push_back(pos+i);
-							is_bad = false;
+							//is_bad = false;
 							if(local_find.count(tmp) == 0)
 								local_find.insert(tmp);
 						}
 					}
 				}
-				if(is_bad)
-					bad.insert(str);
+				//if(is_bad)
+				//	bad.insert(str);
 			}
+			if(this_level)
+				break;
 			pos = pos + size;
 			find.insert(local_find.begin(), local_find.end());
 		}
