d8b6018f19f771c7f16d8ce8a576a9f8e89debc6
sosohu <ustc.sosohu@gmail.com>
2014-09-24 14:30:57 +0800

    v924

--- Word_Ladder_II/main.cc
+++ Word_Ladder_II/main.cc
@@ -23,32 +23,6 @@ void Trace(vector<int>& trace, vector<string>& data, int pos, vector<vector<stri
 		path.push_back(vec);
 	}
 
-	/*
-	void detail(string start, string end, unordered_set<string>& dict, vector<string> trace, vector<vector<string> >path){
-		trace.push_back(start);
-		if(start.compare(end) == 0){
-			path.push_back(trace);
-			return;
-		}
-		int len = start.length();
-		string str;
-		for(int i = 0; i < len; i++){
-			str = start;
-			for(int j = 97; j < 123; j++){
-				str[i] = j;
-				detail(str, end, dict, trace, path);
-			}
-		}
-	}
-
-	vector<vector<string> > findLadders(string start, string end, unordered_set<string> &dict) {
-		vector<vector<string> > path;
-		vector<string> trace;
-		detail(start, end, dict, trace, path);
-		return path;
-	}
-	*/
-
 	vector<vector<string> > findLadders(string start, string end, unordered_set<string> &dict) {
 		int len = start.length();
 		unordered_set<string> find;
@@ -64,20 +38,18 @@ void Trace(vector<int>& trace, vector<string>& data, int pos, vector<vector<stri
 		int length = 0;
 		int size;
 		int pos = 0;
-		//bool is_bad;
 		bool this_level = false;
 		while(size = data.size() - pos){
 			unordered_set<string> local_find;
 			for(int i = 0; i < size; i++){
 				str = data[pos+i];
-				//if(bad.count(str) == 1)
-				//	continue;
 				if(str.compare(end) == 0){
 					Trace(trace, data , pos+i, path);
 					this_level = true;
 					continue;
 				}
-				//is_bad = true;
+				if(this_level)
+					continue;
 				for(int j = 0; j < len; j++){
 				 	tmp = str;
 					for(int k = 97; k < 123; k++){
@@ -85,14 +57,11 @@ void Trace(vector<int>& trace, vector<string>& data, int pos, vector<vector<stri
 						if(dict.count(tmp) == 1 && find.count(tmp) == 0 ){
 							data.push_back(tmp);
 							trace.push_back(pos+i);
-							//is_bad = false;
 							if(local_find.count(tmp) == 0)
 								local_find.insert(tmp);
 						}
 					}
 				}
-				//if(is_bad)
-				//	bad.insert(str);
 			}
 			if(this_level)
 				break;
