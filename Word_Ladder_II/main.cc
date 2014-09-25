807cce0ae9f487496ec969dcd4a0be19c9f060e8
sosohu <ustc.sosohu@gmail.com>
2014-09-25 10:45:00 +0800

    ACCEPT

--- Word_Ladder_II/main.cc
+++ Word_Ladder_II/main.cc
@@ -3,6 +3,7 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,13 +18,16 @@ class Solution {
 		path.push_back(vec);	
 		string tmp;
 		int len;
+		bool ret = false;
 		while(1){
 			int size = path.size();
 			for(int i = 0; i < size; i++){
 				vec = path[i];
 				tmp = vec.back();
-				if(tmp.compare(start) == 0)
-					return;
+				if(tmp.compare(start) == 0){
+					ret = true;
+					break;
+				}
 				vec.push_back(father[tmp][0]);
 				path[i] = vec;
 				len = vec.size();
@@ -32,56 +36,34 @@ class Solution {
 					path.push_back(vec);
 				}
 			}
+			if(ret)
+				break;
 		}
+		
+		for(int i = 0; i < path.size(); i++)
+			reverse(path[i].begin(), path[i].end());
 	}
-
-	/*
-	vector<vector<string> > Trace(string start, string str,
-		   unordered_map<string, vector<string> >& father) {
-		vector<string> vec;
-		vector<vector<string> > path;
-
-		if(start.compare(str) == 0){
-			vec.push_back(str);
-			path.push_back(vec);
-			return path;
-		}
-
-		string tmp;
-		vector<vector<string> > vec_vec;
-		for(int i = 0; i < father[str].size(); i++){
-			tmp = father[str][i];
-			vec_vec = Trace(start, tmp, father);
-			for(int j = 0; j < vec_vec.size(); j++){
-				vec_vec[j].push_back(str);
-				path.push_back(vec_vec[j]);
-			}
-		}
-		return path;
-	}
-	*/
 
 	 vector < vector < string > >findLadders(string start, string end,
 						  unordered_set < string >
 						  &dict) {
 		int len = start.length();
 		unordered_set < string > find;
+		unordered_set < string > last;
+		unordered_set < string > cur;
 		vector < vector < string > > path;
 		unordered_map < string, vector < string > >father;
-		vector < string > data;
 
 		bool this_level = false;
 		find.insert(start);
-		data.push_back(start);
+		last.insert(start);
 		string str, tmp;
-		while (!data.empty()) {
+		while (!last.empty()) {
 
-			int size = data.size();
-			unordered_set < string > cur;
-			vector<string> local_data;
-			for (int k = 0; k < size; k++) {
+			cur.clear();
+			for (auto iter = last.begin(); iter != last.end(); iter++) {
 
-				str = data[k];
+				str = *iter;
 
 				for (int i = 0; i < len; i++) {
 					tmp = str;
@@ -92,7 +74,6 @@ class Solution {
 							father[tmp].push_back(str);
 						}else{
 							if (dict.count(tmp) == 1 && find.count(tmp) == 0) {
-								local_data.push_back(tmp);
 								father[tmp].push_back(str);
 								if(cur.count(tmp) == 0)
 									cur.insert(tmp);
@@ -107,7 +88,7 @@ class Solution {
 				return path;
 			}
 			find.insert(cur.begin(), cur.end());
-			data = local_data;
+			last.swap(cur);
 		}
 		
 		return path;
