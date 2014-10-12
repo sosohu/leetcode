81ccd9bc0a80bc863f1778f3d185a07ba999826a
sosohu <ustc.sosohu@gmail.com>
2014-10-12 13:44:44 +0800

    MLE

--- Wildcard_Matching/main.cc
+++ Wildcard_Matching/main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 
@@ -9,6 +10,65 @@ class Solution {
 
 public:
 
+	bool isMatch(const char *s, const char *p){
+		int len1 = strlen(s);
+		int len2 = strlen(p);
+		const char* p1 = s;
+		const char* p2 = s;
+
+		if(len2 == 0 && len1 != 0)	return false;
+		if(len1 == 0){
+			for(int i = 0; i < len2; i++)	
+				if(p2[i] != '*') return false;
+			return true;
+		}
+
+		vector<vector<bool> > data(len1, vector<bool>(len2, false));
+
+		if(p2[0] == '*'){
+			for(int i = 0; i < len1; i++)
+				data[i][0] = true;
+		}else{
+			if(p2[0] == '?'){
+				data[0][0] = true;
+				for(int i = 1; i < len1; i++)
+					data[i][0] = false;
+			}
+			else{
+				for(int i = 0; i < len1; i++)
+					data[i][0] = false;
+				if(p1[0] == p2[0])
+					data[0][0] = true;
+			}
+		}
+
+		for(int j = 1;  j < len2; j++){
+			switch(p2[j]){
+				case '*': data[0][j] = true; break;
+				case '?': ;
+				default: data[0][j] = false;
+			}
+		}
+
+		int part = false;;
+		for(int j = 1; j < len2; j++){
+			for(int i = 1; i < len1; i++){
+				// assgin data[i][j] 
+				switch(p2[j]){
+					case '?': data[i][j] = data[i-1][j-1]; break;
+					case '*':   part = false;
+								for(int k = i; k >= 0; k--){
+									part = part || data[k][j-1];			
+								}
+								data[i][j] = part; break;
+					default: data[i][j] = p1[i] == p2[j] && data[i-1][j-1];
+				}
+			}
+		}
+		return data[len1-1][len2-1];
+	}
+
+	/*
 	bool isMatch(const char *s, const char *p) {
 		int len1 = strlen(s);
 		int len2 = strlen(p);
@@ -51,7 +111,7 @@ public:
 			return true;
 		}
 		return ret;
-    }
+    }*/
 
 };
 
@@ -59,8 +119,8 @@ int main(int argc, char** argv)
 {
 	Solution sl;
 	
-    bool ret = sl.isMatch("aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba", "a*******b");
-	//bool ret = sl.isMatch("hi","*?");
+    //bool ret = sl.isMatch("aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba", "a*******b");
+	bool ret = sl.isMatch("hia","*?a");
 	
 	cout<<"Result  :("<<ret<<")"<<endl;
 
